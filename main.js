9a2c60192119361e50b63df4d9f6f4cb7067a52b
Victoria Konkiel <victoria.konkiel@yahoo.com>
2020-11-24 14:16:24 -0500

    part 2 question 1 completed

---
+++ main.js
@@ -0,0 +1,36 @@
+// Part 2 Question 1
+
+let myArray = ['Deloris', 'Tajanae', 'Victoria']
+//myArray.splice(myArray.length - 2, 1)
+//console.log(myArray);
+
+function removeSecondToLast(a) {
+    a.splice(a.length - 2, 1)
+}
+
+removeSecondToLast(myArray);
+console.log(myArray);
+
+//Question 2 
+let alpha = [58, 96, 37, 68, 53];
+let beta = [];
+for ( let value of alpha) {
+    beta = alpha.push(value)
+}
+
+console.log(beta);
+
+// var first =  [1, 2, 3];
+// var second = [4, 5];
+ 
+// for (var i of second) {
+//     first.push(i);
+// }
+ 
+// console.log(first);
+ 
+/*
+    Output: [ 1, 2, 3, 4, 5 ]
+*/
+
+//Given two arrays alpha and beta, copy all the values of alpha into beta.
